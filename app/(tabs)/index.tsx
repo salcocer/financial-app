7b7dd6b4bcadf1e07f441db0cc3bc5376aea7a9e
Alejandro Alcocer <saavalencia97@gmail.com>
2026-09-22 12:54:23 +0200

    add style font to the othe pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,39 +6,45 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
     <SafeAreaView className="flex-1  bg-background p-5">
-      <Text className="text-5xl font-sans-extra-bold text-success">Home</Text>
+      <Text className="text-5xl font-sans-extra-bold text-black">Home</Text>
 
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-semibold"
       >
         Go to Onboarding
       </Link>
 
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-semibold"
       >
         Go to Sign In
       </Link>
 
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-semibold"
       >
         Go to Sign Up
       </Link>
 
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      {/* <Link
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-semibold"
+        href="/subscriptions/spotify"
+      >
+        Spotify Subscription
+      </Link>
 
       <Link
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-semibold"
         href={{
           pathname: "/subscriptions/[id]",
           params: { id: "claude-max" },
         }}
       >
         Claude Max Subscription
-      </Link>
+      </Link> */}
     </SafeAreaView>
   );
 }
