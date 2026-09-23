23ba017191824390373214c040b03412219565e9
Alejandro Alcocer <saavalencia97@gmail.com>
2026-09-23 17:25:18 +0200

    refactor: improve sign-in error handling and format code; update user display in App component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
 import ListHeading from '@/components/ListHeading';
 import SubscriptionCard from '@/components/SubscriptionCard';
 import UpcomingSubscriptionCard from '@/components/UpcomingSubscriptionCard';
-import {
-    HOME_BALANCE,
-    HOME_SUBSCRIPTIONS,
-    HOME_USER,
-    UPCOMING_SUBSCRIPTIONS,
-} from '@/constants/data';
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, UPCOMING_SUBSCRIPTIONS } from '@/constants/data';
 import { icons } from '@/constants/icons';
 import images from '@/constants/images';
 import { formatCurrency } from '@/lib/utlis';
+import { useUser } from '@clerk/expo';
 import dayjs from 'dayjs';
 import React, { useState } from 'react';
 import { FlatList, Image, Text, View } from 'react-native';
@@ -17,6 +13,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
     const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<string | null>(null);
+    const { user } = useUser();
+    const displayName = user?.firstName || user?.fullName || 'there';
+    console.log({ user });
 
     return (
         <SafeAreaView className="flex-1 bg-background  p-5">
@@ -25,8 +24,11 @@ export default function App() {
                     <>
                         <View className="home-header">
                             <View className="home-user">
-                                <Image source={images.avatar} className="home-avatar" />
-                                <Text className="home-user-name">{HOME_USER.name}</Text>
+                                <Image
+                                    source={user?.imageUrl ? { uri: user.imageUrl } : images.avatar}
+                                    className="home-avatar"
+                                />
+                                <Text className="home-user-name">{displayName}</Text>
                             </View>
                             <Image source={icons.add} className="home-add-icon" />
                         </View>
